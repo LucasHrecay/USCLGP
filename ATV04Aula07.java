f9cd2687767f64e6e6d41ddbbe736a833988926e
LucasHrecay <164430506+LucasHrecay@users.noreply.github.com>
2024-04-03 22:16:25 -0300

    Update ATV04Aula07.java

--- ATV04Aula07.java
+++ ATV04Aula07.java
@@ -1,69 +1,38 @@
 package ATV;
 import java.util.Scanner;
-public class Atv05 {
+
+public class Atv03 {
 	public static void main (String[]args) {
-		Scanner scanner = new Scanner (System.in);
+		// Esse programa diz o dia da semana
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite um numero de 1 a 7");
+			int num = scanner.nextInt();
+		switch (num) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda-feira");
+			break;
+		case 3:
+			System.out.println("Terça-feira");
+			break;
+		case 4:
+			System.out.println("Quarta-feira");
+			break;
+		case 5:
+			System.out.println("Quinta-feira");
+			break;
+		case 6:
+			System.out.println("Sexta-feira");
+			break;
+		case 7:
+			System.out.println("Sábado");
+			break;
 		
-	System.out.println("Digite o numero de um mês");
-	int mes = scanner.nextInt();
-	
-	switch(mes) {
-	case 1: {
-		System.out.println("Jan");
-		break;
-	}
-	case 2: {
-		System.out.println("Fev");
-		break;
-	}
-	case 3: {
-		System.out.println("Mar");
-		break;
-	}
-	case 4: {
-		System.out.println("Abr");
-		break;
-	}
-	case 5: {
-		System.out.println("Mai");
-		break;
-	}
-	case 6: {
-		System.out.println("Jun");
-		break;
-	}
-	case 7: {
-		System.out.println("Jul");
-		break;
-	}
-	case 8: {
-		System.out.println("Ago");
-		break;
-	}
-	case 9: {
-		System.out.println("Set");
-		break;
-	}
-	case 10: {
-		System.out.println("Out");
-		break;
-	}
-	case 11: {
-		System.out.println("Nov");
-		break;
-	}
-	case 12: {
-		System.out.println("Dez");
-		break;
-	}
-	
-	}
-	
-	if (mes >12) {
-		System.out.println("Digite um numero entre 1 e 12");
-	}
-	else {
-		System.out.println("Digite um numero maior que zero e no maximo 12");
-	}
-	}
+			
+		}
+			
+			
+	}		
 }
