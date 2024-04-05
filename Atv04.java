529447a4381659f56e5d8c2bd6b72a032daa9d9e
LucasHrecay <164430506+LucasHrecay@users.noreply.github.com>
2024-04-04 23:45:51 -0300

    Create Atv04.java

---
+++ Atv04.java
@@ -0,0 +1,15 @@
+package ATV;
+import java.util.Scanner;
+public class Atv04 {
+	public static void main (String []args) {
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Digite um numero");
+		
+			int num = scanner.nextInt();
+			
+			while(num !=0) {
+				System.out.println(num);
+			}
+		
+	}
+}
